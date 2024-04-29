eeb46d397cad36c9c0d02721d21e44e3191f38a1
JahidulSany <jahidulsanypro@gmail.com>
2024-04-29 17:16:28 +0100

    Fetching products by Axios and useQuery

--- use-tanstack-query/src/App.jsx
+++ use-tanstack-query/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Products from './components/Products'
 
 function App() {
 
   return (
     <>
-     <h1 className='text-center test-3xl'>React Tanstack Query Project</h1>
+     <Products />
     </> 
   )
 }
